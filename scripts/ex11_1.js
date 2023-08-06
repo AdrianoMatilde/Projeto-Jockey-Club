4b86ac9617b876a96573e15603725cdb56e33ee4
Francisco Nolasco <franknol-17@hotmail.com>
2023-08-06 17:49:36 -0300

    Finalizada Função do botão apostar

--- scripts/ex11_1.js
+++ scripts/ex11_1.js
@@ -1,17 +1,12 @@
 const frm = document.querySelector("form");
 const respLista = document.querySelector("pre");
-const respCavalo = document.querySelector("#outCacalo");
+const respCavalo = document.querySelector("#outCavalo");
 
 //Nome dos cavalos participantes no páreo
 const CAVALOS = ["Marujo", "Tordilho", "Belga", "Twister", "Jaade", "Lucky"];
 
 //vetor que irá aramazenar um Objeto aposta(com nº do cavalo e valor da aposta)
 const apostas = [];
-
-const obterCavalo = (num) =>{
-    const posicao = num -1; //posição no vetor
-    return CAVALOS[posicao];
-}
 
 frm.addEventListener("submit",(e)=>{
     e.preventDefault();
@@ -48,8 +43,57 @@ frm.inCavalo.addEventListener("blur",()=>{
     if(!validarCavalo(numCavalo)){
         alert("Nº do cavalo inválido");
         frm.inCavalo.focus();
+        frm.inCavalo.value = "";
         return;
     };
 
+    const nome = obterCavalo(numCavalo); // atribui retorno das funções à variáveis
+    const contaNum = contarApostas(numCavalo);
+    const total = totalizarApostas(numCavalo);
+
+    //exibe o número de apostas e ototal apostado no cavalo
+    respCavalo.innerText = `${nome} (Apostas: ${contaNum} - R$: ${total.toFixed(2)})`;
+
     
 });
+
+frm.inCavalo.addEventListener("focus",()=>{
+    frm.inCavalo.value ="";
+    respCavalo.innerText = "";
+});
+
+const obterCavalo = (num) =>{
+    const posicao = num -1; //posição no vetor
+    return CAVALOS[posicao];
+}
+
+const validarCavalo = (num) =>{
+    //retorna o valor resultante da condição (true ou false)
+    return num >= 1 && num <=CAVALOS.length; 
+};
+
+const contarApostas = (num) =>{
+    let contador = 0;
+    //percorre o vetor appstas
+    for (const aposta of apostas) {
+        //verifica se a aposta é no cavalo passado como parâmetro
+        if(aposta.cavalo == num){
+            contador ++; //conta +1 quando a aposta for no cavalo do parametro
+        }
+    }
+
+    return contador; // retorna o nº de apostas no cavalo numCavalo
+};
+
+const totalizarApostas = (num) =>{
+    let total = 0;
+    for(const aposta of apostas) {
+        if(aposta.cavalo == num){
+            total += aposta.valor;//soma o valor das apostas
+        }
+    }
+
+    return total; //retorna os valores apostados em um cavalo
+};
+
+
